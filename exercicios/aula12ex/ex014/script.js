afaba47a569eea0061d58f35982e20e157f15388
LucasBueno2726 <140419660+lucas2726@users.noreply.github.com>
2023-09-04 14:24:50 -0400

    ex

---
+++ exercicios/aula12ex/ex014/script.js
@@ -0,0 +1,18 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById(`img`)  
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+     img.src = 'imagens/manha.png'
+     document.body.style.background = '#218e00'
+    } else if (hora >= 12 && hora <= 18) {
+       img.src = 'imagens/tarde.png'
+       document.body.style.background = '#fa8b46'
+    } else {
+       img.src = 'imagens/noite.png'
+       document.body.style.background = '#000e64'
+    }
+}
+
