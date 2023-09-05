de8d549751555831bd052e94695ee606338e8d3a
LucasBueno2726 <140419660+lucas2726@users.noreply.github.com>
2023-09-05 10:33:44 -0400

    aula13

---
+++ exercicios/aula13/ambiente.js
@@ -0,0 +1,23 @@
+var c = 1
+do {
+    console.log('Tudo bem?')
+    c++
+} while (c <= 6)
+
+/*
+var c = 1
+while (c <= 6) {
+    console.log('Tudo bem?')
+    c++
+}
+*/
+
+// pior comando para quando for repetir algo
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
