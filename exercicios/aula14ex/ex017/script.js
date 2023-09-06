4c370e187d1dc58eef34875fa2c5439f1e4b3f06
LucasBueno2726 <140419660+lucas2726@users.noreply.github.com>
2023-09-06 11:00:49 -0400

    1

--- exercicios/aula14ex/ex017/script.js
+++ exercicios/aula14ex/ex017/script.js
@@ -1,1 +1,22 @@
-var inicio = 
+function tabuada() {
+    var num = document.getElementById('txtn')
+    var tab = document.getElementById('seltab')
+    /* conecta o caixa número com a resposta */
+
+    if (num.value.length == 0) { /*Para caso não coloque nada emita uma mensagem de alerta*/
+        window.alert('Por favor, digite um número!')
+    } else {
+        var n = Number(num.value)
+        var c = 1
+        tab.innerHTML = '' //Para limpar a caixa de texto
+        while (c <= 10) /*Para ir de 1 até 10 */ {
+            var item = document.createElement('option') //Para conectar com o option do html
+            item.text = `${n} x ${c} = ${n*c}` //Para fazer a conta de multiplicação
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    } 
+}  
+
+
