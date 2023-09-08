30836a94ae795d7ae63cfdab2574d98926c998bf
LucasBueno2726 <140419660+lucas2726@users.noreply.github.com>
2023-09-08 12:15:17 -0400

    ex

--- exercicios/aula16ex/ex018/script.js
+++ exercicios/aula16ex/ex018/script.js
@@ -1,32 +1,56 @@
-let num = document.getElementById('txtn')
-let tab = document.getElementById('seltab')
-let res = document.getElementById('res')
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []
 
+function isNumero(n) {
+   if (Number(n) >= 1 && Number(n) <= 100) {
+    return true
+   } else {
+    return false
+   }  
+} 
+
+function inLista(n, l) {
+   if (l.indexOf(Number(n)) != -1) {
+    return true
+   } else {
+    return false
+   }
+}
 
 function adicionar() {
-    if (num.value.length == 0 || num.value > 100) {
-      window.alert('Valor inválido ou já encontrado na lista')
-    } else {
-      let n = Number(num.value)
+  if (isNumero(num.value) && !inLista(num.value, valores)) {
+      valores.push(Number(num.value))
       let item = document.createElement('option')
-      item.text = `Valor ${n} adicionado`
-      tab.appendChild(item)
-    }
+      item.text = `Valor ${num.value} adicionado.`
+      lista.appendChild(item)
+      num.value = '' //Para esvaziar a caixa toda
+   } else {
+       window.alert('Valor inválido ou já encontrado na lista.')
+  }
+  num.value = '' //Para esvaziar a caixa do número
+  num.focus() /*Para o cursos ficar piscando para adicionar outro número sem tocar denovo*/
 }
 
 function finalizar() {
-  if (num.value.length == 0) {
-    window.alert('Adicione valores antes de começar')
+  if (valores.length == 0) {
+    window.alert('Adicione valores antes de finalizar.')
   } else {
-    
-    res.innerHTML = `Ao todo, temos ${} números cadastrados. <br>
-    O maior valor informado foi ${}<br>
-    O menor valor informado foi ${} <br>
-    Somando todos os valores, temos ${} <br>
-    A média dos valores digitados é ${} `
-
-    
+    let tot = valores.length /*Para dizer a quantidade de números dentro da lista*/
+    let maior = valores[0]
+    let menor = valores[0]
+    for (let pos in valores) {
+      if (valores[pos] > maior)
+      maior = valores[pos]
+      if (valores[pos] < menor)
+          menor = valores[pos]
+    }
+    res.innerHTML = '' //Para esvaziar a caixa do res
+    res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados<p>`
+    res.innerHTML += `<p>O maior valor informado foi ${}<p>`
+    res.innerHTML += `<p><p>`
+    res.innerHTML += `<p><p>`
   }
 }
-
 
