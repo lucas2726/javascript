44f053c546e21dd7bdf5e5ae173ba64ddd5c933d
LucasBueno2726 <140419660+lucas2726@users.noreply.github.com>
2023-09-15 17:02:16 -0400

    ex

---
+++ exercicios/aula27/index.js
@@ -0,0 +1,6 @@
+let num1 = 9.8383838
+//let num2 = Math.floor(num1)
+//let num2 = Math.ceil(num1)
+//let num2 = Math.round(num1)
+console.log(Math.max(1,2,3,4,5,6))
+console.log(Math.min(1,2,3,4,5,6))
---
+++ exercicios/aula32/index.js
@@ -0,0 +1,6 @@
+//                      1              2               3
+ //               1    2    3     1    2   3     1     2    3
+const numeros = [[100, 200, 300], [400, 500, 600], [700, 800, 900]]
+const [lista1, lista2, lista3] = numeros
+console.log(lista1[2])
+console.log(lista1)
