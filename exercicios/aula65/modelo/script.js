8c2bdde1c133260df4c3a6e1a77d868bbe5e1572
LucasBueno2726 <140419660+lucas2726@users.noreply.github.com>
2023-09-19 10:51:12 -0400

    ex

--- exercicios/aula65/modelo/script.js
+++ exercicios/aula65/modelo/script.js
@@ -1,4 +1,38 @@
+function criaHoraDosSegundos(segundos) {
+    const data = new Date(segundos * 1000)
+    return data.toLocaleTimeString('pt-br', {hour12: false, timeZone: 'UTC'})
+}
+
 const relogio = document.querySelector('.relogio')
-const iniciar = document.querySelector('#iniciar')
-const pausar = document.querySelector('#pausar')
-const zerar = document.querySelector('#zerar')
+let segundos = 0
+let timer
+
+function iniciaRelogio() {
+    timer = setInterval(function() {
+    segundos++
+    relogio.innerHTML = criaHoraDosSegundos(segundos)
+   }, 1000)
+}
+
+document.addEventListener('click', function(event) {
+    const elemento = event.target
+
+    if(elemento.classList.contains('iniciar')) {
+        relogio.classList.remove('pausado')
+        clearInterval(timer)
+        iniciaRelogio()
+    }
+
+    if(elemento.classList.contains('pausar')) {
+        clearInterval(timer)
+        relogio.classList.add('pausado')
+    }
+
+    if(elemento.classList.contains('zerar')) {
+        clearInterval(timer)
+    relogio.innerHTML = '00:00:00'
+    segundos = 0
+    }
+})
+
+
