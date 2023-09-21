bf4e92a4a66c3d6276a1bffb4335e05c2ddabe55
LucasBueno2726 <140419660+lucas2726@users.noreply.github.com>
2023-09-20 21:21:21 -0400

    ex

---
+++ exercicios/aula68/index.js
@@ -0,0 +1,5 @@
+function funcao(a, b, c, d, e) {
+   
+}
+
+funcao(1, 2, 3, 4, 5, 6, 7)
---
+++ exercicios/aula69/index.js
@@ -0,0 +1,7 @@
+function criaPessoa(nome,sobrenome) {
+    return {nome, sobrenome}
+}
+
+const p1 = criaPessoa('Luiz', 'Otávio')
+
+console.log(p1)
