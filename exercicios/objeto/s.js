13da5f71cc6b2488f9c66fcad4816b9204780685
LucasBueno2726 <140419660+lucas2726@users.noreply.github.com>
2023-09-25 16:56:21 -0400

    ex

--- exercicios/objeto/s.js
+++ exercicios/objeto/s.js
@@ -1,15 +1,20 @@
-// Objeto pai (protótipo)
-var animal = {
-    speak: function() {
-      console.log("O animal faz um som.");
-    }
-  };
+const frutas = ["maçã", "banana"];
+   const novoComprimento = frutas.push("laranja", "morango");
+   console.log(frutas); // Isso imprimirá ["maçã", "banana", "laranja", "morango"]
+   console.log(novoComprimento); // Isso imprimirá 4 (o novo comprimento do array)
+
+
+
+
+
+
+
+
+
+
   
-  // Objeto filho que herda do objeto pai
-  var gato = Object.create(animal);
-  
-  // Agora, o objeto gato tem acesso ao método speak do objeto animal
-  gato.speak(); // Isso imprimirá "O animal faz um som."
+
+
   
 
 
