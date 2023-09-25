9401fdc0fa690a6fc74ff2f545be2c88925ea90c
LucasBueno2726 <140419660+lucas2726@users.noreply.github.com>
2023-09-24 20:35:59 -0400

    ex

---
+++ exercicios/objeto/s.js
@@ -0,0 +1,11 @@
+const objetoDestino = { a: 1, b: 2 };
+const objetoOrigem = { b: 3, c: 4 };
+
+// Copia as propriedades do objetoOrigem para o objetoDestino
+Object.assign(objetoDestino, objetoOrigem);
+
+console.log(objetoDestino)
+
+
+
+
