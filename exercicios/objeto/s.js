420fc1d60c1a9f4cf1d1b7d4173eaf1d5fffccf1
LucasBueno2726 <140419660+lucas2726@users.noreply.github.com>
2023-09-25 10:43:54 -0400

    ex

--- exercicios/objeto/s.js
+++ exercicios/objeto/s.js
@@ -1,10 +1,20 @@
-const objetoDestino = { a: 1, b: 2 };
-const objetoOrigem = { b: 3, c: 4 };
+// Objeto pai (protótipo)
+var animal = {
+    speak: function() {
+      console.log("O animal faz um som.");
+    }
+  };
+  
+  // Objeto filho que herda do objeto pai
+  var gato = Object.create(animal);
+  
+  // Agora, o objeto gato tem acesso ao método speak do objeto animal
+  gato.speak(); // Isso imprimirá "O animal faz um som."
+  
 
-// Copia as propriedades do objetoOrigem para o objetoDestino
-Object.assign(objetoDestino, objetoOrigem);
 
-console.log(objetoDestino)
+
+
 
 
 
