ca2313a6a317fd578c49e5bf530fa07169b28d3d
LucasBueno2726 <140419660+lucas2726@users.noreply.github.com>
2023-10-11 19:42:15 -0400

    ex

--- exercicios/objeto/s.js
+++ exercicios/objeto/s.js
@@ -1,3 +1,19 @@
+const fs = require('fs');
+
+// Dados que você quer escrever no arquivo
+const dadosParaEscrever = 'Olá, mundo!';
+
+// Caminho para o arquivo onde você quer escrever os dados
+const caminhoArquivo = 'caminho/do/seu/arquivo.txt';
+
+// Escreve os dados no arquivo de forma assíncrona
+fs.writeFile(caminhoArquivo, dadosParaEscrever, (err) => {
+  if (err) {
+    console.error('Erro ao escrever no arquivo:', err);
+    return;
+  }
+  console.log('Dados foram escritos no arquivo com sucesso.');
+});
 
 
 
