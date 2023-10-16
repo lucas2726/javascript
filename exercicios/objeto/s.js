9a48b9af85c87c5481036187190e5f36e729be71
LucasBueno2726 <140419660+lucas2726@users.noreply.github.com>
2023-10-16 11:00:18 -0400

    ex

--- exercicios/objeto/s.js
+++ exercicios/objeto/s.js
@@ -1,19 +1,13 @@
-const fs = require('fs');
+// Middleware de Logging
+function loggingMiddleware(req, res, next) {
+  // Registra os detalhes da solicitação
+  console.log(`Método: ${req.method}, URL: ${req.url}`);
+  // Chama o próximo middleware na cadeia ou o controlador final
+  next();
+}
 
-// Caminho para o arquivo que você quer ler
-const caminhoArquivo = 'caminho/do/seu/arquivo.txt';
-
-// Lê o arquivo de forma assíncrona (não bloqueante)
-fs.readFile(caminhoArquivo, 'utf8', (err, dados) => {
-  if (err) {
-    console.error('Erro ao ler o arquivo:', err);
-    return;
-  }
-  console.log('Conteúdo do arquivo:', dados);
-});
-
-console.log('Lendo o arquivo...'); // Esta linha será impressa antes do conteúdo do arquivo devido à natureza assíncrona da leitura de arquivos.
-
+// Uso do middleware de logging em uma rota específica
+app.use('/rota-especifica', loggingMiddleware);
 
 
 
