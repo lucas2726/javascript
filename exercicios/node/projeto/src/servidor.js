2340a3c9f4b005628bc1545d49d90e62757bd65e
LucasBueno2726 <140419660+lucas2726@users.noreply.github.com>
2023-10-16 15:45:12 -0400

    ex

--- exercicios/node/projeto/src/servidor.js
+++ exercicios/node/projeto/src/servidor.js
@@ -2,9 +2,31 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const bancoDeDados = require('./bancoDeDados')
+
+app.use(bodyParser.urlencoded({ extend: true }))
 
 app.get('/produtos', (req, res, next) => {
     res.send({nome: 'Notebook', preco: 123.45})
+})
+
+app.get('/produtos/:id', (req, res, next)=> {
+    res.send(bancoDeDados.getProdutos(req.params.id))
+})
+
+app.put('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto) //JSON
+})
+
+app.delete('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.excluirProduto(req.params.id)
+    res.send(produto) 
 })
 
 app.listen(porta, () => {
