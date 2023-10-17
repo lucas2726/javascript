58ec1bfc6a01750b40ba4efcf090b037b4b462e7
LucasBueno2726 <140419660+lucas2726@users.noreply.github.com>
2023-10-17 10:57:08 -0400

    ex

--- exercicios/esnext/revisao.js
+++ exercicios/esnext/revisao.js
@@ -1,3 +1,14 @@
+async function obterDadosDaAPI(url) {
+    try {
+      let resposta = await fetch(url);
+      let dados = await resposta.json();
+      console.log("Dados da API:", dados);
+    } catch (erro) {
+      console.error("Erro ao obter dados da API:", erro);
+    }
+  }
+  
+
 
 
 
