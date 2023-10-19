bf77a6b597bd987ecd1dcf8459ffc9719fd6e501
LucasBueno2726 <140419660+lucas2726@users.noreply.github.com>
2023-10-19 11:18:59 -0400

    ex

--- exercicios/ajax/server.js
+++ exercicios/ajax/server.js
@@ -2,9 +2,30 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 
-app.use(express.static('.'))
+app.use(express.static('./upload'))
 app.use(bodyParsee.urlencoded({ extennded: true}))
 app.use(bodyParser.json())
 
-app.get('/teste', (req, res) => res.send('Ok'))
-app.listen(8080, console.log('Executando...'))
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, '.')
+    },
+    filename: function(req, file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({ storage }).single('arquivo')
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if (err) {
+            return res.end('ERRO')
+        }
+        res.end('Concluido')
+    })
+})
+
+app.listen(8080, () => console.log('Executando...'))
--- exercicios/objeto/oo.js
+++ exercicios/objeto/oo.js
@@ -1,7 +1,25 @@
-//Abstração
-//Encapsulamento
-//Herança
-//Polimorfismo
+fetch('https://api.example.com/data')
+  .then(function(response) {
+    return response.json(); // Converte a resposta em formato JSON
+  })
+  .then(function(data) {
+    console.log(data); // Faça algo com os dados recebidos, por exemplo, atualizar a interface do usuário.
+  })
+  .catch(function(error) {
+    console.error('Erro na requisição:', error); // Trata erros de rede ou outras falhas
+  });
+
+
+
+
+
+
+
+
+
+
+
+  
 
 
   
