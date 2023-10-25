e3a4feff662131c2bbca9827e2b36749d7603427
LucasBueno2726 <140419660+lucas2726@users.noreply.github.com>
2023-10-25 17:52:24 -0400

    ex

--- exercicios/objeto/oo.js
+++ exercicios/objeto/oo.js
@@ -1,15 +1,22 @@
+const formData = new FormData();
 
-function MinhaFuncao() {
-  this.propriedade = "valor";
-}
+// Adicionando dados ao objeto FormData usando append()
+formData.append('nome', 'João');
+formData.append('email', 'joao@example.com');
 
-const instancia = new MinhaFuncao();
+// Enviando o objeto FormData via fetch
+fetch('https://api.exemplo.com/salvar', {
+  method: 'POST',
+  body: formData
+})
+.then(response => response.json())
+.then(data => {
+  console.log(data); // Manipula a resposta do servidor
+})
+.catch(error => {
+  console.error('Erro:', error); // Trata erros de requisição
+})
 
-console.log(instancia.propriedade); // Saída: "valor"
-
-delete instancia.propriedade; // Remove a propriedade do objeto
-
-console.log(instancia.propriedade); // Saída: undefined, pois a propriedade foi removida
 
 
 
